c2e4b897d5d10135cb26c1b4b7f1b9b69972e96c
Veasna <Veasna3d@gmail.com>
2025-04-19 10:30:58 +0700

    Loging Screen

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.Hello</Text>
+      <Text style={styles.title}>Testing</Text>
+      <Link href='/(auth)/signup'>Signup</Link>
+      <Link href='/(auth)'>Login</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: 'blue'
+  }
+})
