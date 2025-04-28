19f5a877664c84468228cecc1fa9f9249e5b8c1d
Veasna <Veasna3d@gmail.com>
2025-04-28 14:20:05 +0700

    Login

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,19 +1,26 @@
-import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator, KeyboardAvoidingView, Platform } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator, KeyboardAvoidingView, Platform, Alert } from 'react-native'
 import styles from '@/assets/styles/login.styles'
 import React, { useState } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import COLORS from '@/constants/colors'
 import { Link, useRouter } from 'expo-router'
+import { useAuthStore } from '@/store/authStore'
 
 export default function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
 
-  const handleLogin = () => {
-    console.log('login..');
+  const { isLoading, login } = useAuthStore()
+
+  const handleLogin = async () => {
+    const result = await login({ email, password });
+    if (!result.success) {
+      Alert.alert('Error', result.error);
+    } else {
+      Alert.alert('Success', 'Login successfully!');
+    }
   }
 
   return (
--- mobile/store/authStore.ts
+++ mobile/store/authStore.ts
@@ -7,11 +7,25 @@ export type RegisterProps = {
     email: string;
 };
 
+type AuthUser = {
+    username: string;
+    email: string;
+    token: string;
+};
+
+type LoginProps = {
+    email: string;
+    password: string;
+}
+
 type AuthState = {
-    user: string | null;
+    user: AuthUser | null;
     token: string | null;
     isLoading: boolean,
     register: (args: RegisterProps) => Promise<{ success: boolean; error?: string }>;
+    login: (args: LoginProps) => Promise<{ success: boolean; error?: string }>;
+    checkAuth: () => Promise<void>;
+    logout: () => Promise<void>;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
@@ -19,7 +33,7 @@ export const useAuthStore = create<AuthState>((set) => ({
     token: null,
     isLoading: false,
 
-    register: async ({ username, password, email }: { username: string, password: string, email: string }) => {
+    register: async ({ username, password, email }: RegisterProps) => {
         try {
             set({ isLoading: true })
             const res = await fetch(`https://react-native-qdcu.onrender.com/api/auth/register`, {
@@ -49,5 +63,53 @@ export const useAuthStore = create<AuthState>((set) => ({
             set({ isLoading: false })
             return { success: false, error: error.message }
         }
+    },
+
+    login: async ({ email, password }: LoginProps) => {
+        set({ isLoading: true })
+        try {
+
+            const res = await fetch(`https://react-native-qdcu.onrender.com/api/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                })
+            })
+
+            const data = await res.json()
+            if (!res.ok) throw new Error(data.message || 'Something went wrong')
+
+            await AsyncStorage.setItem('user', JSON.stringify(data.user))
+            await AsyncStorage.setItem('token', data.token)
+
+            set({ token: data.token, user: data.user, isLoading: false })
+            return { success: true }
+
+        } catch (error: any) {
+            set({ isLoading: false })
+            return { success: false, error: error.message }
+        }
+    },
+
+    checkAuth: async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const userJson = await AsyncStorage.getItem('user');
+            const user: AuthUser | null = userJson ? JSON.parse(userJson) : null;
+
+            set({ token, user })
+        } catch (error) {
+            console.log('Auth check failed', error);
+        }
+    },
+
+    logout: async () => {
+        await AsyncStorage.removeItem('token')
+        await AsyncStorage.removeItem('user')
+        set({ token: null, user: null })
     }
 }));
