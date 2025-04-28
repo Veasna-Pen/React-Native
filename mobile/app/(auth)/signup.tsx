c123f6c11111843e3236da2549182918ac10f40c
Veasna <Veasna3d@gmail.com>
2025-04-28 11:47:53 +0700

    improve

--- mobile/app/(auth)/signup.tsx
+++ mobile/app/(auth)/signup.tsx
@@ -11,7 +11,7 @@ export default function Signup() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
-  const { user, isLoading, register } = useAuthStore();
+  const { user, isLoading, register, token } = useAuthStore();
 
   const handleSignup = async () => {
     ;
